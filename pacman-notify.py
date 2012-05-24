c960442c9012a6b42becbfc649c75b700de35629
Kevin Casucci <kcasucci@gmail.com>
2012-05-23 23:06:48 -0400

    switch out notification scripts

--- pacman-notify.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import os
-
-os.system("sudo pacman -Sy")
-packages = os.popen("pacman -Qu | egrep -vi '^(Checking|warning|Remove|Total)'").read().strip()
-packages = ", ".join(packages.splitlines())
-if packages != "":
-    os.system("echo 'Pacman Updates: "+ packages +"' | dzen2 -p 20 -bg '#dc322f'")
